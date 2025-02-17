bf08f245cf132b1fa0fccdc61319d56e8c860b0e
brunofigueh <bruno_alqui@hotmail.com>
2025-02-17 11:06:15 -0300

    Loops
    Introdução While, for  e Do while

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
@@ -21,13 +23,53 @@ public class HelloWorld {
 		 */
 		Scanner input = new Scanner(System.in);
 		
-		int idade;
+		int idade = 15;
 		
-		System.out.println("Escreva sua idade: ");
+		String nome;
 		
-		idade = input.nextInt();
+		//while 
+		System.out.println(" ------------ Whiel ---------------");
 		
-		System.out.println("Minha idade é " + idade);
+		int contador = 1;
+		
+		while(contador <= 10) {
+			System.out.println(contador);
+			contador++;
+		}
+		/*
+		 * Usamos geralmente quando é indefinido o tempo de execução do loop
+		 */
+		
+		//for
+		System.out.println("----------- For ---------------");
+		
+		for(int cont=1; cont<=10; cont++){
+			System.out.println(cont);
+		}
+		
+		/*
+		 * Usamos for quando sabemos quando termina o loop geralmente
+		 */
+		
+		//System.out.println("Escreva sua idade: ");
+		
+		//idade = input.nextInt();
+		
+		//System.out.println("Minha idade é " + idade);
+		
+		
+		/*while(idade < 18) {
+			System.out.println("É menor de idade. ");
+			idade++;
+		}
+		System.out.println("Maior de idade. "); */
+		System.out.println("Insira seu nome: ");
+		nome = input.nextLine();
+		
+		while(nome.equals("".strip())) {
+			System.out.println("Você precisa escrever seu nome: ");
+			nome = input.nextLine();
+		}
 	}
 
 }
